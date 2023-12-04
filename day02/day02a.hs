edeb180cc35f64d4e2ceb00b3cac053feae9087c
Gethin Davies <grd.davies@gmail.com>
2023-12-04 19:32:18 +0000

    ParseInput module

--- day02/day02a.hs
+++ day02/day02a.hs
@@ -1,58 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Char (isDigit)
-import Data.Maybe (catMaybes, fromMaybe, isNothing)
-import Data.Text (Text)
+import Data.Maybe (catMaybes, isNothing)
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
-import Distribution.Compat.Prelude (readMaybe)
-import Text.Read (readMaybe)
-
-data Game = Game
-  { gameId :: Int,
-    sets :: [Sample]
-  }
-  deriving (Show)
-
-parseGame :: Text -> Maybe Game
-parseGame line = do
-  let foo = T.dropWhile (not . isDigit) line
-  gameId <- readMaybe . T.unpack $ T.takeWhile isDigit foo
-  sets <- parseSamples $ T.dropWhile (not . isDigit) $ T.dropWhile (' ' /=) foo
-  return Game {gameId = gameId, sets = sets}
-
-type Sample = [Count]
-
-parseSamples :: Text -> Maybe [Sample]
-parseSamples str = traverse parseSample $ T.splitOn "; " str
-
-parseSample :: Text -> Maybe Sample
-parseSample str = do
-  let counts = T.splitOn ", " str
-  traverse parseCount counts
-
-data Count = Count
-  { count :: Int,
-    colour :: Colour
-  }
-  deriving (Show)
-
-parseCount :: Text -> Maybe Count
-parseCount str = do
-  let [h, t] = T.splitOn " " str
-  count <- readMaybe $ T.unpack h
-  colour <- parseColour t
-  return Count {count = count, colour = colour}
-
-data Colour = Red | Green | Blue
-  deriving (Show)
-
-parseColour :: Text -> Maybe Colour
-parseColour str = case str of
-  "red" -> Just Red
-  "green" -> Just Green
-  "blue" -> Just Blue
-  _ -> Nothing
+import ParseInput
 
 countPossible :: Count -> Bool
 countPossible x = case colour x of
